ec50a29ed7c3416764e30c7cb61986c14f9e8871
mhegde <mhegde@broadinstitute.org>
2017-06-22 17:30:12 -0400

    updated code

--- hypergeom_1.5.2.py
+++ hypergeom_1.5.2.py
@@ -5,7 +5,7 @@ Performs hypergeometric analysis for genetic perturbation screens
 Input: 1. Input file with column headers
        2. Chip File
        3. Option to include/exclude singletons in output file;Default: Singletons included
-       4. Option to specify mean/median of LFC and p-values;Default: Mean
+       4. Option to specify n% for mean p-val, LFC calculation
 '''
 import pandas as pd
 import numpy as np
@@ -27,10 +27,10 @@ def get_parser():
         type=str,
         default='Y',
         help='Y to include genes with single perturbations in output file, N to exclude; Default: Y')
-    parser.add_argument('--pval',
-        type=str,
-        default='mean',
-        help='\'mean\' for displaying average p-values and \'median\' for median;default is mean')
+    parser.add_argument('--fraction',
+        type=int,
+        default=100,
+        help='Fraction of perturbations to be included in mean LFC, pval calculation, 1 - 100; Default: 100')
     return parser
 
 '''
@@ -51,7 +51,7 @@ def sort_reindex(df, col, direction):
         sys.exit(1)
     return df
 
-def calc_hypergeom_scores(merged, st_in, ge, pval_type):
+def calc_hypergeom_scores(merged, st_in, ge, frac):
     grouped = merged.groupby('Gene Symbol')
     sps = list(st_in['Guide Sequence'])
     tot_sps = len(sps)
@@ -78,12 +78,9 @@ def calc_hypergeom_scores(merged, st_in, ge, pval_type):
         #op_lfcs = ';'.join([str(l) for l in lfcs])
         all_ranks = ';'.join(str(r) for r in rank)
         p_values = [-log10(stats.hypergeom.pmf(rank.index(x)+1, tot_sps, length_sps, x)) for x in rank]
-        if pval_type == 'mean' or pval_type == 'Mean':
-            avg_p_val = np.mean(p_values)
-            avg_lfc = np.mean(lfcs)
-        elif pval_type == 'median' or pval_type == 'Median':
-            avg_p_val = np.median(p_values)
-            avg_lfc = np.median(lfcs)
+        num_perts = int(frac/100.0*length_sps)
+        avg_p_val = np.mean(p_values[0:num_perts])
+        avg_lfc = np.mean(lfcs[0:num_perts])
         op_p_vals = ';'.join("%.2E" %Decimal(p) for p in p_values)
         g_p_val[g] = op_lfcs+'_'+str(avg_lfc)+'_'+op_p_vals+'_'+str(avg_p_val)+'_'+guide_list+'_'+all_ranks
     return g_p_val
@@ -103,9 +100,13 @@ if __name__ == '__main__':
     ref_colnames[0:2] = ['Guide Sequence', 'Gene Symbol']
     ref.columns = ref_colnames
     include = args.sing_pert
-    pval_type = args.pval
+    frac = args.fraction
+    inputname = inputfile.split('/')[-1]
+    o_folder = 'Volcano-plots_'+inputname[:-4]+'_'+str(datetime.now().strftime("%y-%m-%d-%H-%M-%S"))
+    if not os.path.exists(o_folder):
+        os.makedirs(o_folder)
     for ci,c in enumerate(cols_iter):
-        outputfile = c+'_Output_'+str(datetime.now().strftime("%y-%m-%d-%H-%M"))+'.txt'
+        outputfile = o_folder+'/'+c+'_'+str(frac)+'_'+str(datetime.now().strftime("%y-%m-%d-%H-%M"))+'.txt'
         st_in = input_df[['Guide Sequence',c]]
         st_in = st_in.rename(columns={c:'Score'})
         merged = pd.merge(st_in, ref, on='Guide Sequence')
@@ -114,31 +115,34 @@ if __name__ == '__main__':
 
         merged = sort_reindex(merged, 'Score', 'P')
         st_in = sort_reindex(st_in, 'Score', 'P')
-        g_p_val_P = calc_hypergeom_scores(merged, st_in, ge, pval_type)
+        g_p_val_P = calc_hypergeom_scores(merged, st_in, ge, frac)
         merged = sort_reindex(merged, 'Score', 'N')
         st_in = sort_reindex(st_in, 'Score', 'N')
-        g_p_val_N = calc_hypergeom_scores(merged, st_in, ge, pval_type)
+        g_p_val_N = calc_hypergeom_scores(merged, st_in, ge, frac)
 
         with open(outputfile,'w') as o:
             w = csv.writer(o, delimiter='\t', lineterminator='\n')
-            if pval_type == 'mean' or pval_type == 'Mean':
-                w.writerow(('Gene Symbol', 'Average LFC', 'Average -log(p-values)', 'Number of perturbations', 'Perturbations', 'Individual LFCs', 'Ascending ranks','Individual ascending -log(p-values)', 'Descending ranks', 'Individual descending -log(p-values)'))
-            elif pval_type == 'median' or pval_type == 'Median':
-                w.writerow(('Gene Symbol', 'Median LFC', 'Median -log(p-values)', 'Number of perturbations', 'Perturbations', 'Individual LFCs', 'Ascending ranks','Individual ascending -log(p-values)', 'Descending ranks', 'Individual descending -log(p-values)'))
-            print 'Analyzing ...'
+            w.writerow(('Gene Symbol', 'Average LFC', 'Average -log(p-values)', 'Number of perturbations', 'Perturbations', 'Individual LFCs', 'Ascending ranks','Individual ascending -log(p-values)', 'Descending ranks', 'Individual descending -log(p-values)'))
+            print 'Analyzing '+c
             for g in ge:
                 in_lfcs, avg_lfc, p_p_vals, avg_p_val, guide_list, ranks_p = g_p_val_P[g].split('_')
                 in_lfcs, avg_lfc, n_p_vals, avg_n_val, guide_list, ranks_n = g_p_val_N[g].split('_')
 
-                if avg_p_val > avg_n_val:
-                    avg_p_val = avg_p_val
+                if float(avg_p_val) > float(avg_n_val):
+                    avg_p_val = float(avg_p_val)
                 else:
-                    avg_p_val = avg_n_val
+                    avg_p_val = float(avg_n_val)
                 if include == 'N':
                     if len(guide_list.split(';')) != 1:
-                        w.writerow((g, avg_lfc, avg_p_val, len(in_lfcs.split(';')), guide_list, in_lfcs, ranks_p, p_p_vals, ranks_n, n_p_vals))
+                        w.writerow((g, avg_lfc, avg_p_val, len(in_lfcs.split(';')), guide_list, in_lfcs, ranks_n, n_p_vals, ranks_p, p_p_vals))
                 else:
-                    w.writerow((g, avg_lfc, avg_p_val, len(in_lfcs.split(';')), guide_list, in_lfcs, ranks_p, p_p_vals, ranks_n, n_p_vals))                    
-
+                    w.writerow((g, avg_lfc, avg_p_val, len(in_lfcs.split(';')), guide_list, in_lfcs, ranks_n, n_p_vals, ranks_p, p_p_vals))  
+    with open(o_folder+'/README.txt','w') as o:
+        w = csv.writer(o,delimiter='\t')
+        w.writerow((['Code Version: 1.5.2']))
+        w.writerow((['Input file:'+inputfile]))
+        w.writerow((['Chip file:'+args.chip_file]))
+        w.writerow((['Output folder:'+o_folder]))
+        w.writerow((['Fraction of perturbations for mean p-value, LFC:'+str(frac)]))
                 
 
